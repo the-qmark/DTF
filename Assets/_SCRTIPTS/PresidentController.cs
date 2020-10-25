9327d3ee92d8052c93072acf7cf7a8230cd5ff84
the-qmark <egormalyukow@gmail.com>
2020-10-25 23:12:43 +0400

    Начало положено

---
+++ Assets/_SCRTIPTS/PresidentController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class PresidentController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    [SerializeField] float speedOfWalk;
+
+    private float currentSpeedOfWalk;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * currentSpeedOfWalk * Time.deltaTime);
+    }
+
+    public void GameStart()
+    {
+        animator.SetBool("GameStart", true);
+        currentSpeedOfWalk = speedOfWalk;
+    }
+
+    public void GameOver()
+    {
+        animator.SetBool("GameOver", true);
+    }
+}
